e501e20d0da0587316f357893c32471511dfc821
JUNAID HISHAM <junaidhisham1234@gmail.com>
2022-05-22 17:29:16 +0530

    updated db

--- src/dict/search/urls.py
+++
@@ -1,9 +0,0 @@
-# from search import views
-# from django.urls import path
-
-# app_name = "search"
-
-# urlpatterns = [
-#     path('',views.index, name="index"),
-#     path('add_word/', views.create_word, name="create_word"),
-# ]
